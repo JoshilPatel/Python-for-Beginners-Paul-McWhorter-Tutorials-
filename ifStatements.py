9a8928aa211bb2d8e85fa8e8a35dbff127dfbe0a
JoshilPatel <144334639+JoshilPatel@users.noreply.github.com>
2024-07-15 17:30:16 +0100

    Add files via upload

---
+++ ifStatements.py
@@ -0,0 +1,14 @@
+"""myColour=input("Please enter your colour: ")
+
+if (myColour=="red"):
+    print("Your colour is red")
+
+if (myColour!="red"):
+    print("Your colour is not red")"""
+
+myNumber = int(input("Please enter your number: "))
+if (myNumber==7):
+    print("Your number is 7")
+
+if(myNumber!=7):
+    print("Your number is not 7")
